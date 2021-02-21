328e0ec9c5f514f772ae044d9a7e6ff928f9920d
Jesus Pacheco <jemanuelpachecoo@gmail.com>
2021-02-21 03:09:32 -0300

    Construido algoritmo ramificacion y poda

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
+import dominio.ElementoPatron;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         RamificacionYPoda ramificacionYPoda = new RamificacionYPoda();
 
-        ramificacionYPoda.podar(147,new double[]{1,1.2f,1.4f,1.6f,1.8f,2,2.2f,2.4f,2.6f,2.8f,3});
+        double[] li = new double[]{1,1.2f,1.4f,1.6f,1.8f,2,2.2f,2.4f,2.6f,2.8f,3};
+        double menor = Arrays.stream(li).
+                filter(i -> i != 0)
+                .min()
+                .orElse(0);
+//        ramificacionYPoda.podar(147,);
+//        int numero = 4;
+//        ArrayList<Integer> numeros = new ArrayList<>();
+//        ramificacionYPoda.combinaciones(numero,numeros,0);
+        ArrayList<ArrayList<ElementoPatron>> esquemas = new ArrayList<>();
+        ramificacionYPoda.ramificar(esquemas, 147.2,(double)0, new ArrayList<>(),li,menor,0);
+        System.out.println(esquemas);
     }
 }
--- src/main/java/dominio/ElementoPatron.java
+++ src/main/java/dominio/ElementoPatron.java
@@ -1,8 +1,24 @@
 package dominio;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ElementoPatron {
     private Integer cantidad;
     private Double sumaMedida;
+    private Double medida;
+    DecimalFormat df = new DecimalFormat("#.00");
+
+    public ElementoPatron() {
+    }
+
+    public ElementoPatron(Integer cantidad, Double sumaMedida, Double medida) {
+        this.cantidad = cantidad;
+        this.sumaMedida = sumaMedida;
+        this.medida = medida;
+    }
 
     public Integer getCantidad() {
         return cantidad;
@@ -20,5 +36,28 @@ public class ElementoPatron {
     public ElementoPatron setSumaMedida(Double sumaMedida) {
         this.sumaMedida = sumaMedida;
         return this;
+    }
+
+    public Double getMedida() {
+        return medida;
+    }
+
+    @Override
+    public String toString() {
+        return
+//                "cantidad=" +
+                cantidad +
+                        "-" +
+//                ", sumaMedida=" + sumaMedida +
+//                ", medida=" +
+                df.format(medida) + " : ";
+    }
+
+    public static Double sumaTotal(ArrayList<ElementoPatron> elementos){
+        Double total = 0.0;
+        for (ElementoPatron elemento:elementos) {
+            total += elemento.getSumaMedida();
+        }
+        return total;
     }
 }
